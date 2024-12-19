47fc86e44dfe8bab433fcab175b7a7301bbebf12
dev78972343 <developer78972343@gmail.com>
2024-12-19 20:26:47 +0600

    Added styles for list items

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,10 +1,16 @@
+import { Editor } from "./Editor";
+
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
-  return <div>DocumentIdPage: {documentId}</div>;
+  return (
+    <div className="bg-[#F9FBFD]">
+      <Editor />
+    </div>
+  );
 };
 
 export default DocumentIdPage;
--- src/app/documents/settings/page.tsx
+++ src/app/documents/settings/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Settings page content</p>
+    </div>
+  );
 };
 
 export default SettingsPage;
